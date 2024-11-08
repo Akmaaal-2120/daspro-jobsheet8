b7480fce716d0bfe9b589f690a817cd4040dbcc2
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-11-08 22:15:17 +0700

    Assignment

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class assignment2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        String name, sports, poly;
+
+        System.out.print("Enter the number of polytechnic register : ");
+        int participant = sc.nextInt();
+        sc.nextLine();
+        for(int i = 1; i <= participant; i++){
+            System.out.print("Enter your Polytechnic : ");
+            poly = sc.nextLine();
+            for(int n = 1; n <= 4; n++){
+                System.out.print("Enter your Sports : ");
+                sports = sc.nextLine();
+                System.out.println("Athlete for " + sports);
+                for(int m = 1; m <= 5; m++){
+                    System.out.print("Enter your Athlete : ");
+                    name = sc.nextLine();
+                    System.out.println("Polytechnic : " + poly + "\nName : " + name);
+                }
+            }
+        }
     }
 }
